d0a8c4e524d09fc733679cc9ed82ec5cbb216d01
Joe Gasiorek <joe.gasiorek@gmail.com>
2014-05-17 09:39:25 -0700

    try this

--- goal.php
+++ goal.php
@@ -1,8 +1,8 @@
 <?php
 require('vendor/autoload.php');
 
-$response = Bernoulli\Client::GetExperiments('first', $_GET['id'], null);
-echo $response;
+$response = Bernoulli\Client::GoalAttained('first', $_GET['id'], null);
+echo json_encode($response);
 echo "\n";
 
 echo 'done!';
--- index.php
+++ index.php
@@ -2,13 +2,13 @@
 require('vendor/autoload.php');
 
 $experiments = Bernoulli\Client::GetExperiments('first', null, [], null);
-echo json_encode($experiments);
+echo "Response: " . echo json_encode($experiments);
 echo "\n";
 if ($experiments[0]['variant'] == "hello_world") {
-	echo "hello world!";
+	echo "hello world!\n";
 } else {
-	echo "goodbye world!";
+	echo "goodbye world!\n";
 }
 
 ?>
-<a href="goal.php?id=<?php echo $experiments[0]['user_id'] ?>">goal</a>
+<a href="goal.php?id=<?php echo $experiments[0]['user_id'] ?>">Goal</a>
